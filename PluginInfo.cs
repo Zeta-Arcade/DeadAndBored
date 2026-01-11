42d30f3ea59079b42ec504ffb69144ee4f8655ab
Zeta Arcade <zZETAz@outlook.com>
2026-01-11 17:22:41 +0000

    Whoops, updated Vers. No.

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -10,6 +10,6 @@ namespace DeadAndBored
 
         public const string PLUGIN_NAME = "DeadAndBoredFork";
 
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.1.0";
     }
 }
--- obj/Debug/netstandard2.1/PluginInfo.cs
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -4,6 +4,6 @@ namespace DeadAndBored
     {
         public const string PLUGIN_GUID = "DeadAndBored";
         public const string PLUGIN_NAME = "DeadAndBored";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.0.1";
     }
 }
