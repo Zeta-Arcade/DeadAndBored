abc1fceaeba037f17a4b262b3aae7c425f4d1f0b
Zeta Arcade <zZETAz@outlook.com>
2026-01-27 11:03:52 +0000

    Cleaned up project references

--- obj/Release/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace DeadAndBored
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "DeadAndBored";
-        public const string PLUGIN_NAME = "DeadAndBored";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
